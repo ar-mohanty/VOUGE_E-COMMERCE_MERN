0ffb3c1b5d7740dc17c3168301fe89ff300cf66a
Anurag Mohanty <68963714+AR-Mohanty@users.noreply.github.com>
2023-10-20 18:52:16 +0530

    #2: Login / Signup and React-Router Setup
    - Login Page Setup
    - Signup Page Setup
    -Login & Signup Component Setup in features

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React from "react";
 import "./App.css";
 import { Counter } from "./features/counter/Counter";
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage/>,
+  },
+]);
 
 function App() {
   return (
     <>
-      <Home></Home>
+      <RouterProvider router={router} />
     </>
   );
 }
